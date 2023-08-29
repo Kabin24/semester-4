63c07bbc8e5c19d324b34312512d1a9cb4dd5f38
kabin shrestha <108089715+Kabin24@users.noreply.github.com>
2023-08-29 23:06:48 +0545

    update

--- Advanced programming/Javacode/src/Week5/assignment/Display.java
+++ Advanced programming/Javacode/src/Week5/assignment/Display.java
@@ -1,13 +1,11 @@
 package Week5.assignment;
+
 import java.util.HashSet;
 import java.util.LinkedHashSet;
+
 public class Display {
 
-
-
-
-
-        Main main = new Main();
+    Main main = new Main();
 
         public static void DisplayItems(String[] ds) {
             System.out.println("Available Items: ");
@@ -21,8 +19,29 @@ public class Display {
             }
 
         }
-
-        
+    public void checkCondition(HashSet<String> items_new,String user_selected_item){
+        // checking value duplicate then, yeuta statement | choina aarko
+        if(items_new.contains(user_selected_item)){
+            System.out.println("Item " + "\'" + user_selected_item + "\'" + " is already to added to your cart." );
         }
+        else{
+
+            // user ley select gareko item lai hashset mha added.
+            items_new.add(user_selected_item);
+
+            System.out.println("Item " + "\'" + user_selected_item + "\'" + "added to your cart." );
+        }
+
     }
+    public static void displayUserShopping (HashSet<String> userselected) {
+        for (String it : userselected) {
+            System.out.println("- " + it);
+        }
+
+
+    }
+}
+
+
+
 
--- Advanced programming/Javacode/src/Week5/assignment/Main.java
+++ Advanced programming/Javacode/src/Week5/assignment/Main.java
@@ -25,8 +25,58 @@ public class Main {
         items[3] = "Backpack";
         items[4] = "Hat";
 
-        // Display available item to user
         display.DisplayItems(items);
 
+        Scanner src = new Scanner(System.in);
+        HashSet<String> items_new = new HashSet<>();
+
+
+        int i = 0;
+        int user_input;
+
+        while(i<=7){
+
+            System.out.println("Select an item to add to your cart (enter item number): ");
+            user_input = src.nextInt();
+
+
+            if(user_input == 0 || user_input>items.length){
+                System.out.println("You entered wrong choice. Reentered...");
+                continue;
+            }
+
+            String user_selected_item = items[user_input-1];
+
+            display.checkCondition(items_new, user_selected_item);
+
+            i++;
+
+            if(i==7){
+
+                System.out.println();
+                System.out.println("Would you like to view your cart? (Y/N): ");
+                String userViewChoice = src.next();
+
+                if(userViewChoice.equals("Y")){
+                    System.out.println();
+                    System.out.println("Items in Your Cart: ");
+
+                    display.displayUserShopping(items_new);
+
+                }
+
+                if(userViewChoice != "Y"){
+                    System.out.println("Thanks for your shopping !!");
+                }
+
+
+            }
+
+
+        }
+
+
     }
+
+
 }
