a243608cb737b57012905bfa8abced151a52173e
kabin shrestha <108089715+Kabin24@users.noreply.github.com>
2023-08-29 23:43:01 +0545

    update

--- Advanced programming/Javacode/src/Week5/assignment/Display.java
+++ Advanced programming/Javacode/src/Week5/assignment/Display.java
@@ -20,13 +20,13 @@ public class Display {
 
         }
     public void checkCondition(HashSet<String> items_new,String user_selected_item){
-        // checking value duplicate then, yeuta statement | choina aarko
+
         if(items_new.contains(user_selected_item)){
             System.out.println("Item " + "\'" + user_selected_item + "\'" + " is already to added to your cart." );
         }
         else{
 
-            // user ley select gareko item lai hashset mha added.
+            
             items_new.add(user_selected_item);
 
             System.out.println("Item " + "\'" + user_selected_item + "\'" + "added to your cart." );
