85cdc701d717fc350cfb3ed5d85c7bdde7ab615e
kabin shrestha <108089715+Kabin24@users.noreply.github.com>
2023-09-20 15:07:14 +0545

    updated

--- Advanced programming/Javacode/src/oopinjava/encapsulation.java
+++ Advanced programming/Javacode/src/oopinjava/encapsulation.java
@@ -1,5 +1,24 @@
 package oopinjava;
 
 public class encapsulation {
+    private String name;  // private = restricted access
 
+    //Getter
+    public String getName() {
+        return name;
+    }
+
+    // setter
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+
+
+    public static void main(String[] args) {
+        encapsulation myObj = new encapsulation();
+        myObj.setName("kirtan");
+        myObj.setName("kabin");
+        System.out.println(myObj.name);
+    }
 }
