3f97a745bcdf8d285fdb49c8e0cfd2b5911a1380
kabin shrestha <108089715+Kabin24@users.noreply.github.com>
2023-09-29 22:56:35 +0545

    new commit

--- Advanced programming/Javafx_/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.javafx_ {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens com.example.javafx_ to javafx.fxml;
-    exports com.example.javafx_;
-}
